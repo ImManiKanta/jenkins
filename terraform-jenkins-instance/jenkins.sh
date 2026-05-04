070d58ac797c1bc08f8920b2b17691648a8b39ce
ImManiKanta <chinthamanikanta1@gmail.com>
2026-05-04 13:41:18 +0530

    jenkins updated

--- terraform-jenkins-instance/jenkins.sh
+++ terraform-jenkins-instance/jenkins.sh
@@ -4,7 +4,7 @@ sudo growpart /dev/nvme0n1 4
 
 sudo lvextend -L +10G /dev/RootVG/rootVol
 sudo lvextend -L +10G /dev/RootVG/varVol
-sudo lvextend -L +10G /dev/RootVG-homeVol 
+sudo lvextend -L +10G /dev/mapper/RootVG-homeVol
 
 sudo xfs_growfs /
 sudo xfs_growfs /var
